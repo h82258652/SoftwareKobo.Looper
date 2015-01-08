a14df15c94e62b4b74845a278921b57e3bc8949e
h82258652 <842053625@qq.com>
2015-01-08 21:29:42 +0800

    添加一个WPF的demo

---
+++ SoftwareKobo.Looper/SoftwareKobo.Looper.WPFDemo/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+
+namespace SoftwareKobo.WPFDemo
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnClick(object sender, RoutedEventArgs e)
+        {
+            VisualTreeLooper looper = new VisualTreeLooper(this);
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("层级|控件名");
+            looper.Loop((manager, obj) => sb.AppendFormat("{0}|{1}{2}", manager.Deep, obj, Environment.NewLine), null);
+            MessageBox.Show(sb.ToString());
+        }
+    }
+}
---
+++ SoftwareKobo.Looper/SoftwareKobo.Looper.WPFDemo/VisualTreeLooper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+
+namespace SoftwareKobo.WPFDemo
+{
+    public class VisualTreeLooper
+    {
+        private Looper _looper;
+
+        public VisualTreeLooper(DependencyObject obj)
+        {
+            _looper = new Looper(obj, GetChildren, HasChildren);
+        }
+
+        public void Loop(Action<LoopStateManager, DependencyObject> beforeLoopChildren, Action<LoopStateManager, DependencyObject> afterLoopChildren)
+        {
+            _looper.Loop((arg1, arg2) =>
+            {
+                if (beforeLoopChildren != null)
+                {
+                    beforeLoopChildren(arg1, (DependencyObject)arg2);
+                }
+            }, (arg1, arg2) =>
+            {
+                if (afterLoopChildren != null)
+                {
+                    afterLoopChildren(arg1, (DependencyObject)arg2);
+                }
+            });
+        }
+
+        private IEnumerable<object> GetChildren(object arg)
+        {
+            var obj = (DependencyObject)arg;
+            var childCount = VisualTreeHelper.GetChildrenCount(obj);
+            for (int i = 0; i < childCount; i++)
+            {
+                yield return VisualTreeHelper.GetChild(obj, i);
+            }
+        }
+
+        private bool HasChildren(object arg)
+        {
+            var obj = (DependencyObject)arg;
+            return VisualTreeHelper.GetChildrenCount(obj) > 0;
+        }
+    }
+}
